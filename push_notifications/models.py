e97da2dbe341f3c84534ca16109cc184e827e35f
Philip Kimmey <philip.kimmey@gmail.com>
2013-01-27 11:52:59 -0800

    Add framework for switching on device type

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -4,15 +4,13 @@ from people.models import Person
 from .managers import DeviceManager
 from .managers import IDeviceManager
 
+from .api import apn as apn_api
+from .api import gcm as gcm_api
+
 APP_ID_CHOICES = (
     ('com.rover.Rover-com', 'com.rover.Rover-com'),
 )
 
-
-DEVICE_TYPE_CHOICES = (
-    ('android', 'Android'),
-    ('ios', 'iOS')
-)
 
 
 class DeviceBase(models.Model):
@@ -20,27 +18,42 @@ class DeviceBase(models.Model):
 
     is_development = models.BooleanField(default=False)
 
+    DEVICE_TYPE_IOS = 'ios'
+    DEVICE_TYPE_ANDROID = 'android'
+    DEVICE_TYPE_CHOICES = (
+        (DEVICE_TYPE_ANDROID, 'Android'),
+        (DEVICE_TYPE_IOS, 'iOS')
+    )
     device_type = models.CharField(
         max_length=10,
         choices=DEVICE_TYPE_CHOICES)
+
     device_id = models.CharField(
         max_length=64,
         blank=False,
         unique=True)
     registration_id = models.TextField(
         blank=True,
+        unique=True)
 
     added_at = models.DateTimeField(default=timezone.now)
     modified_at = models.DateTimeField(auto_now=True)
 
     is_active = models.BooleanField(
         default=True)
+    invalidated = models.BooleanField(
+        default=False)
 
     class Meta:
         abstract = True
 
     def send_message(self, message):
-        pass
+        if self.device_type == self.DEVICE_TYPE_IOS:
+            return apn_api.send_message(message)
+        elif self.device_type == self.DEVICE_TYPE_ANDROID:
+            return gcm_api.send_message(message)
+        else:
+            raise InvalidDeviceTypeException()
 
 
 class IDevice(models.Model):
@@ -51,6 +64,3 @@ class IDevice(models.Model):
 
     token = models.CharField(max_length=64,
             default='',null=False, blank=False, unique=True)
-    app_id = models.CharField(max_length=64,
-            default='',null=False, blank=False,
-            verbose_name='The token is invalidated by APN')

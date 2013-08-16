b261949166926897a4e2e2bf413ced22ff8d5b22
Johannas Heller <johann@phyfus.com>
2013-08-16 16:21:58 -0700

    Do not send messges to inactive or invalidated devices.

--- device_notifications/tests/model_tests.py
+++ device_notifications/tests/model_tests.py
@@ -12,17 +12,17 @@ class ConcreteTestDevice(AbstractBaseDevice):
     pass
 
 
-class AbstractBaseDeviceTests(TestCase):
+class AbstractBaseDeviceSendMessageTests(TestCase):
     def setUp(self):
         self.get_device_model_patcher = patch.object(
             settings,
             'get_device_model',
             return_value=ConcreteTestDevice)
         self.get_device_model_patcher.start()
-        super(AbstractBaseDeviceTests, self).setUp()
+        super(AbstractBaseDeviceSendMessageTests, self).setUp()
 
     def tearDown(self):
-        super(AbstractBaseDeviceTests, self).tearDown()
+        super(AbstractBaseDeviceSendMessageTests, self).tearDown()
         self.get_device_model_patcher.stop()
 
     @patch('device_notifications.models.gcm_send_message_task')
@@ -37,10 +37,37 @@ class AbstractBaseDeviceTests(TestCase):
         gcm_send_message_task.apply_async.assert_called_with(
             args=[device.pk, message])
 
-    @patch('device_notifications.models.gcm_send_message_task')
-    def test_send_message_bad_device_type(self, gcm_send_message_task):
-        device = ConcreteTestDevice(
-            pk=1,
-            device_type='windows_phone')
+    def test_bad_device_type(self):
+        device = ConcreteTestDevice(device_type='windows_phone')
 
         self.assertRaises(InvalidDeviceType, device.send_message, 'Hi')
+
+    @patch('device_notifications.models.gcm_send_message_task')
+    def test_does_not_send_to_invalidated_devices(self, gcm_send_message_task):
+        device = ConcreteTestDevice(
+            device_type='android',
+            invalidated=True)
+
+        device.send_message('test')
+
+        self.assertFalse(gcm_send_message_task.apply_async.called)
+
+    @patch('device_notifications.models.gcm_send_message_task')
+    def test_does_not_send_to_inactive_devices(self, gcm_send_message_task):
+        device = ConcreteTestDevice(
+            device_type='android',
+            is_active=False)
+
+        device.send_message('test')
+
+        self.assertFalse(gcm_send_message_task.apply_async.called)
+
+    @patch('device_notifications.models.gcm_send_message_task')
+    def test_does_not_send_to_inactive_and_invalidated_devices(self, gcm_send_message_task):
+        device = ConcreteTestDevice(
+            device_type='android',
+            is_active=False)
+
+        device.send_message('test')
+
+        self.assertFalse(gcm_send_message_task.apply_async.called)
